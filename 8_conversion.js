0179f693affd41dca1ae090a120ad4470c27972e
ashim <magarashim69086@gmail.com>
2024-09-09 10:55:03 +0545

    managed

---
+++ 8_conversion.js
@@ -0,0 +1,13 @@
+//here we will know about the conversion of variable into string and number or boolean.
+let x="5";//this variable consists of string 5 we convert it into number or integer type;
+x=Number(x);
+let y=8;
+console.log(x+y);
+
+y=String(y);//convert integer type to string type;
+console.log(x+y);
+
+
+
+
+//coersion skipped//
---
+++ 9_opeartor/9_typeofoperator.js
@@ -0,0 +1,6 @@
+let x = 9;
+console.log(typeof x) //  'number'
+
+console.log(typeof "albert") // 'string'
+
+console.log(typeof true) // 'boolean

3707ca5d09ea9674d2ad6c134e3b2d331fab56f4
ashim <magarashim69086@gmail.com>
2024-09-09 17:04:01 +0545

    modified

--- 9_opeartor/9_logical_operator.js
+++
@@ -1,5 +0,0 @@
-console.log(true && false);
-
-console.log(false || false);
-
-console.log(!true);
--- 9_opeartor/9_typeofoperator.js
+++
@@ -1,6 +0,0 @@
-let x = 9;
-console.log(typeof x) //  'number'
-
-console.log(typeof "albert") // 'string'
-
-console.log(typeof true) // 'boolean

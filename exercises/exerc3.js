4c38ce2f51dd22970d043157dfcf6ba77e3bbc18
ashim <magarashim69086@gmail.com>
2024-09-09 16:44:04 +0545

    exercise 3 added

---
+++ exercises/exerc3.js
@@ -0,0 +1,6 @@
+console.log(19 % 3);//1
+console.log(10 == 3);//false
+console.log(10 !== "10");//true(compares string also)
+console.log(2 < "10");//true(compares string also)
+console.log("5" > 2);//true(compares string also)
+console.log((false && true) || false);//true

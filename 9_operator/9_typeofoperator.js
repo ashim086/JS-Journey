3eb122ecccb07814c5b7b70743832bdbac79b0df
ashim <magarashim69086@gmail.com>
2024-09-09 16:55:50 +0545

    type of operatoradded

---
+++ 9_operator/9_typeofoperator.js
@@ -0,0 +1,7 @@
+//type of operator defines what is the type of data type it is holding;
+let x = 9;
+console.log(typeof x) //  'number'
+
+console.log(typeof "albert") // 'string'
+
+console.log(typeof true) // 'boolean

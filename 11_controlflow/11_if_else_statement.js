438a4725e917e9ee29bc30bf4285ae067eb550b0
ashim <magarashim69086@gmail.com>
2024-09-10 21:43:13 +0545

    contro flow started

---
+++ 11_controlflow/11_if_else_statement.js
@@ -0,0 +1,24 @@
+/*we will learn about the flow of statement to control*/
+let a=5;
+if(a==5){
+    console.log("the above statement is true and matchable");
+}
+else{
+    console.log("the above statement is false and umatchable");
+}
+
+
+
+//another way of implementing if else statement
+
+
+let balance=50001;
+if(balance>50000){
+    console.log("You have more than 50thousand");
+}
+else if(balance==50001){
+    console.log("Your previous balance is as it is");
+}
+else{
+    console.log("Your balance is less");
+}

3accc0d7eac09fc624adfe6f87d34b4a08fe20d2
ashim <magarashim69086@gmail.com>
2024-09-10 22:20:43 +0545

    exercise 5 adde

---
+++ exercises/exerc5.js
@@ -0,0 +1,14 @@
+let grade='A';
+switch(grade){
+    case 'A':
+        console.log("Students that got an A will get a Chocolate");
+        break;
+    case 'B':
+        console.log("Students that got an A will get a Chocolate");
+        break;
+    case 'C':
+        console.log("Students that got an A will get a Chocolate");
+        break;
+    default:
+        console.log("No reward to give");
+}

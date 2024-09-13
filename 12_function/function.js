30989fc5223ff62746017bc7e78a0c8921b6095f
ashim <magarashim69086@gmail.com>
2024-09-13 23:11:44 +0545

    12-started

---
+++ 12_function/function.js
@@ -0,0 +1,6 @@
+//here we will learn about the function used in js and how does it work
+//function is just the feature of oop - encapsulation code are bind inside the function so that we can use it immediately
+function add(a,b){
+    let add=a+b;
+    console.log(add);
+}

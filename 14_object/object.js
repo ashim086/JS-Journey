79f04d4954cfaf77ad9d9aaa5c427ea71bef62b3
ashim <magarashim69086@gmail.com>
2024-09-17 22:36:49 +0545

    object added

---
+++ 14_object/object.js
@@ -0,0 +1,26 @@
+// object = A collection of related properties and/or methods
+// can represent real world objects (people.prodeuct,places)
+// object = {key:CSSMathValue,
+//     function()}
+
+
+
+
+
+
+const identity ={
+name:"ashim",
+status:"student",
+introduce:() =>{
+    console.log("hello my name is ashim thapa and I am learing Java_script_14-object");
+},
+last_name:"thapa"
+
+}
+
+
+
+console.log(identity.name);
+console.log(identity.status);
+identity.introduce();
+console.log(identity.last_name);
